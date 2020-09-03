36f6ea30c473f9effb207d47d8752c3d7a7a3036
yuridido <yuridido@yahoo.it>
2020-09-03 16:23:08 +0200

    argh

--- js/script.js
+++ js/script.js
@@ -16,9 +16,11 @@
 // https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
 
 $(document).ready(function() {
+    // evento tasto ricerca
     $('header #tasto-cerca').click(function() {
         ricerca();
     });
+    // evento invio nella casella input
     $('header #ricerca').keydown(function(event) {
         if (event.which == 13) {
             ricerca();
