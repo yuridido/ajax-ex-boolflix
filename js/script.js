469ea1f2329310dc08dc5553c24f4c98491990cf
yuridido <yuridido@yahoo.it>
2020-09-03 16:07:57 +0200

    doppia chiamata

--- js/script.js
+++ js/script.js
@@ -7,13 +7,21 @@
 // 3. Lingua
 // 4. Voto
 
+// Milestone 2:
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Qui un esempio di chiamata per le serie tv:
+// https://api.themoviedb.org/3/search/tv?api_key=e99307154c6dfb0b4750f6603256716d&language=it_IT&query=scrubs
+
 $(document).ready(function() {
     $('header #tasto-cerca').click(function() {
-        ricercaFilm();
+        ricerca();
     });
     $('header #ricerca').keydown(function(event) {
         if (event.which == 13) {
-            ricercaFilm();
+            ricerca();
         }
     });
 
@@ -23,15 +31,17 @@ $(document).ready(function() {
 
 
     // FUNZIONI
-    function ricercaFilm() {
+    function ricerca() {
         // salvo il contenuto della casella in una variabile
         var testoRicerca = $('header #ricerca').val()
         // controllo il caso di stringa vuota
         if (testoRicerca == "") {
             $('.container').empty();
-            $('.container').html('nessun risultato');
+            noResult();
         } else {
-            // effettuo la chiamata ajax con la query della variabile ricavata
+            // effettuo la chiamata ajax per i film
+            $('.container').empty();
+            $('header #ricerca').val("");
             $.ajax(
                 {
                     url: 'https://api.themoviedb.org/3/search/movie',
@@ -42,16 +52,37 @@ $(document).ready(function() {
                         query: testoRicerca,
                     },
                     success: function(risposta) {
-                        $('.container').empty();
+                        console.log(risposta.results.length);
+                        // controllo con un if se ho risultati utili altrimenti restituisco mess
+                        if (risposta.results.length != 0) {
+                            insertFilm(risposta);
+                        };
+
+                    },
+                    error: function() {
+                        alert('errore');
+                    }
+                }
+            );
+            // effettuo la chiamata ajax per le serie
+            $.ajax(
+                {
+                    url: 'https://api.themoviedb.org/3/search/tv',
+                    method: 'GET',
+                    data: {
+                        api_key: '1328a497ac2df77fd8be609391d51e42',
+                        language: 'it-IT',
+                        query: testoRicerca,
+                    },
+                    success: function(risposta) {
                         console.log(risposta.results.length);
                         // controllo con un if se ho risultati utili altrimenti restituisco mess
                         if (risposta.results.length == 0) {
-                            $('.container').text('non ci sono risultati');
+                            noResult();
                         } else {
-                            insertFilm(risposta);
+                            insertSerie(risposta);
                         }
 
-                        $('header #ricerca').val("");
                     },
                     error: function() {
                         alert('errore');
@@ -70,6 +101,7 @@ $(document).ready(function() {
                 var immagine = "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
             };
             // creo l'oggetto con i dati della chiamata da inserire nel template
+
             var context = {
                 titolo: data.results[i].title,
                 titoloOriginale: data.results[i].original_title,
@@ -85,9 +117,32 @@ $(document).ready(function() {
         }
     }
 
-    function reset() {
-        $('.container').empty();
-        $('header #ricerca').val("");
+    function insertSerie(data) {
+        for (var i = 0; i < data.results.length; i++) {
+            if (data.results[i].poster_path == null) {
+                var immagine = "https://images.pexels.com/photos/2262403/pexels-photo-2262403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+            } else {
+                var immagine = "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
+            };
+            // creo l'oggetto con i dati della chiamata da inserire nel template
+
+            var context = {
+                titolo: data.results[i].name,
+                titoloOriginale: data.results[i].original_name,
+                lingua: flag(data.results[i].original_language),
+                voto: stars(data.results[i].vote_average),
+                img: immagine,
+            }
+
+            var source = $("#entry-template").html();
+            var template = Handlebars.compile(source);
+            var html = template(context);
+            $('.container').append(html);
+        }
+    }
+
+    function noResult() {
+        return $('.container').append('<div class="no-risultato"><span>nessun risultato</span></div>');
     }
 
     function stars(data) {
